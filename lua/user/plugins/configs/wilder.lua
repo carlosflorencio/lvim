baf446497b8f165e4a34a07d6f421704384b0cd5
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-02-22 17:26:32 +0000

    fix: remove wilder for buffer search

--- lua/user/plugins/configs/wilder.lua
+++ lua/user/plugins/configs/wilder.lua
@@ -1,5 +1,5 @@
 local wilder = require("wilder")
-wilder.setup({ modes = { ':', '/', '?' } })
+wilder.setup({ modes = { ':' } }) -- '/', '?'
 -- wilder.set_option('renderer', wilder.popupmenu_renderer({
 --   -- highlighter applies highlighting to the candidates
 --   highlighter = wilder.basic_highlighter(),
